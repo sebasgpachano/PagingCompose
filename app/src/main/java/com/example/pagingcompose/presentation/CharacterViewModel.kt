085140b835218e74278ffc575eb6aec113783a54
Sebastian <sebastiangarciapachano@gmail.com>
2024-09-30 21:18:49 +0200

    character screen design

--- app/src/main/java/com/example/pagingcompose/presentation/CharacterViewModel.kt
+++ app/src/main/java/com/example/pagingcompose/presentation/CharacterViewModel.kt
@@ -1,9 +1,15 @@
 package com.example.pagingcompose.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.paging.PagingData
+import com.example.pagingcompose.data.RickRepository
+import com.example.pagingcompose.presentation.model.CharacterModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 @HiltViewModel
-class CharacterViewModel @Inject constructor() : ViewModel() {
+class CharacterViewModel @Inject constructor(rickRepository: RickRepository) : ViewModel() {
+
+    val characters: Flow<PagingData<CharacterModel>> = rickRepository.getAllCharacters()
 }
