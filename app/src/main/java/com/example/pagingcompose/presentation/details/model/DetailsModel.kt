1391d58c9d12c82e50a8d521c6b75c0f1d717884
Sebastian <sebastiangarciapachano@gmail.com>
2024-10-08 16:24:39 +0200

    DetailsModel

---
+++ app/src/main/java/com/example/pagingcompose/presentation/details/model/DetailsModel.kt
@@ -0,0 +1,10 @@
+package com.example.pagingcompose.presentation.details.model
+
+data class DetailsModel(
+    val id: Int,
+    val name: String,
+    val image: String,
+    val status: String,
+    val species: String,
+    val gender: String
+)
